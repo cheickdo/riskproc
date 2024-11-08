fd63179a274905af454cdbfb6b2c6e5da81a7efa
chk <cheick.a.doumbia@gmail.com>
2024-11-07 23:25:11 -0500

    added machine information registers

---
+++ docs/CSR_table.md
@@ -0,0 +1,13 @@
+
+Currently allocated CSR addresses
+
+Field specification format:
+WIRI: 2'b00
+WPRI: 2'b01
+WLRL: 2'b10
+WARL: 2'b11
+
+
+Number : Privilege : Name : Description
+
+TBD
--- src/csr.v
+++ src/csr.v
@@ -1,7 +1,18 @@
 module csr(
     input clk,
-    input [11:0] csr_addr
+    input [11:0] csr_addr,
+    input [31:0] data_in,
+    output [31:0] csr_readbus
 );
-    reg [11:0] csreg [11:0];
+    reg [11:0] csreg [31:0];
+
+    always@(*)
+        //Machine information registers, READONLY
+        csreg[0xF10]  = 32'b10000000000100000000000100000000;//misa register
+        csreg[0xF11]  = 32'b00000000000000000000000000000000;//mvendorid, non-commercial device
+        csreg[0xF12]  = 32'b00000000000000000000000000000000;//marchid
+        csreg[0xF13]  = 32'b00000000000000000000000000000000;//mimpid
+        csreg[0xF14]  = 32'b00000000000000000000000000000000;//mhartid
+
 
 endmodule

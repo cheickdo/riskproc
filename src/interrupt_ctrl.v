1d227557b3765e034e49fe927201edb66341fb55
chk <cheick.a.doumbia@gmail.com>
2024-11-20 23:55:22 -0500

    started work on interrupt_ctrl module

--- src/interrupt_ctrl.v
+++ src/interrupt_ctrl.v
@@ -1,2 +1,10 @@
-module interrupt_ctrl();
+module interrupt_ctrl(
+    input [31:0] mstatus,
+    input [31:0] mie,
+    input [31:0] mip,
+    input [31:0] mcause
+    //input [31:0] mtvec, should be handled in the processor itself
+    //mepc also handled 1 level above
+
+);
 endmodule

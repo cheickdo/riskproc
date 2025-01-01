5b98fef3129b908d346c8d9a9012b8a682c21ad5
chk <cheick.a.doumbia@gmail.com>
2025-01-01 12:13:43 -0500

    removed redundant comment

--- src/proc.v
+++ src/proc.v
@@ -100,7 +100,7 @@ assign rs3 = IR[31:27];
 assign funct7 = IR[31:25];
 assign I_Imm = IR[31:20];
 assign S_Imm = {IR[31:25], IR[11:7]};
-assign B_Imm = {IR[31], IR[7], IR[30:25], IR[11:8], 1'b0}; //NOT CORRECT
+assign B_Imm = {IR[31], IR[7], IR[30:25], IR[11:8], 1'b0};
 assign UJ_Imm = {IR[31], IR[19:12], IR[20], IR[30:21], 1'b0};
 assign U_Imm = {IR[31:12]};
 
